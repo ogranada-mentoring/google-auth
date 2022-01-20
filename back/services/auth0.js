4718f95de09547e2502296c362623759d669149d
Oscar Andrés Granada Baquero <oscar.andres.granada@gmail.com>
2022-01-19 20:20:47 -0500

    Updates in package.json and added missing files

---
+++ back/services/auth0.js
@@ -0,0 +1,23 @@
+
+const { auth } = require('express-openid-connect');
+
+function initializeAuth0(app) {
+  const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: process.env.AUTH0_LOCAL_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_BASE_URL,
+    secret: process.env.AUTH0_SECRET,
+    // routes: {}
+  };
+  console.log({
+    ...config,
+    secret: `1`
+  })
+  app.use(auth(config));
+}
+
+module.exports = {
+  initializeAuth0
+}
