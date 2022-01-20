971d083e72f91839488f466b947eca50ead1ef67
Oscar Andrés Granada Baquero <oscar.andres.granada@gmail.com>
2022-01-19 19:20:58 -0500

    Changes to use correct service usage

--- back/index.js
+++ back/index.js
@@ -1,28 +1,11 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors')
-const { auth } = require('express-openid-connect');
 const passport = require('passport');
 const initServices = require('./services');
 const initPublicRouter = require('./routes/public');
 const prepareRoutes = require('./routes/auth');
-
-function initializeAuth0(app) {
-  const config = {
-    authRequired: false,
-    auth0Logout: true,
-    baseURL: process.env.AUTH0_LOCAL_URL,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    issuerBaseURL: process.env.AUTH0_BASE_URL,
-    secret: process.env.AUTH0_SECRET,
-    // routes: {}
-  };
-  console.log({
-    ...config,
-    secret: `1`
-  })
-  app.use(auth(config));
-}
+const { initializeAuth0 } = require('./services/auth0');
 
 function main() {
   dotenv.config()
